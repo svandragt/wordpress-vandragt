97350921214573c532c1473e1b752443525467cf
Sander van Dragt <sander@vandragt.com>
2015-07-23 13:13:42 +0100

    FIX multiple category separator

--- format.date.php
+++ format.date.php
@@ -1,3 +1,3 @@
 <p class="meta">
-	<a href="<?php the_permalink();?>"><?php the_time('l, F jS, Y');  ?></a> in <?php the_category(' &gt; ' ); ?> 
+	<a href="<?php the_permalink();?>"><?php the_time('l, F jS, Y');  ?></a> in <?php the_category(', ' ); ?> 
 </p>
