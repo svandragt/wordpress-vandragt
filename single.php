23edae91de35299883c78a039f0eaee3f78c2d24
Sander van Dragt <sander@vandragt.com>
2015-07-24 13:13:55 +0100

    single in container

--- single.php
+++ single.php
@@ -1,10 +1,13 @@
 <?php get_header(); ?>
 
-<div id="content" class="single">
-	<?php the_post(); ?>
-    <?php get_template_part( 'format.single', get_post_format() ); ?>
-    <?php get_template_part( 'comments' ); ?>
-    <?php get_template_part( 'previousnext' ); ?>
+<div class="container">
+	<div id="content" role="main" class="single">
+		<?php the_post(); ?>
+	    <?php get_template_part( 'format.single', get_post_format() ); ?>
+	    <?php get_template_part( 'comments' ); ?>
+	    <?php get_template_part( 'previousnext' ); ?>
+	</div>
 </div>
  
 <?php get_footer(); ?>
+
