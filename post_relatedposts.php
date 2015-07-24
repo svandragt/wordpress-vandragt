c89ed7441bf86eec25ad29b5872cde4c8abfcf20
Sander van Dragt <sander@vandragt.com>
2015-07-24 13:26:15 +0100

    FIX p width and wording related posts

--- post_relatedposts.php
+++ post_relatedposts.php
@@ -1,7 +1,6 @@
 <?php if (is_single()): ?>
-	<div id="related"><h2>Related <em><?php echo vd_first_category_titles() ?></em> posts </h2>
+	<div id="related"><h2>Also in <em><?php echo vd_first_category_titles() ?></em> </h2>
 
-		<!-- <p>Other interesting posts in this category:</p> -->
 		<?php vd_template_post_relatedposts(); ?>
 	</div>
 <?php endif; ?> 
