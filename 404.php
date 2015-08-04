b1dff9ff54951c4ef6d902cc7819f92e795a1727
Sander van Dragt <sander@vandragt.com>
2015-08-04 10:45:04 +0100

    fix error layout

--- 404.php
+++ 404.php
@@ -1,7 +1,12 @@
 <?php get_header(); ?>
-<div id="content" class="error 404">
-	<h2>Oops</h2>
-	<p>The page you're looking for doesn't appear to exist.</p>
-	<p>Take a look at the <a href="<?php bloginfo( 'url' ); ?>">index page</a> to see if you can find what you're after.</p>
+
+<div class="container">
+	<div id="content" role="main" class="error 404">
+		<h2>Oops</h2>
+		<p>The page you're looking for doesn't appear to exist.</p>
+		<p>Take a look at the <a href="<?php bloginfo( 'url' ); ?>">index page</a> to see if you can find what you're after.</p>
+	</div>
 </div>
+ 
 <?php get_footer(); ?>
+
