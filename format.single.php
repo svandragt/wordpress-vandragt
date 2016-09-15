1aa9889e7e8389b882aa42b1126dc4ecb7ba7355
Sander van Dragt <sander@vandragt.com>
2016-09-15 15:12:11 +0000

    article format

--- format.single.php
+++ format.single.php
@@ -1,10 +1,11 @@
 <article class="format <?php echo get_post_format(); ?>">
+	<?php get_template_part('format.date'); ?>
+
 	<h2><?php
 		printf('<a href="%s">%s</a>', get_permalink(),  get_the_title());
 	?></h2>
 
 	<?php
-		get_template_part('format.date');
 		the_content('Continue Reading &raquo;');
 		get_template_part(get_post_type(), 'meta');
 	?>
