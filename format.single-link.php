7a53e61f40828325792052d542e8e623537b68d1
Sander van Dragt <sander@vandragt.com>
2016-09-16 08:59:39 +0000

    restore page

--- format.single-link.php
+++ format.single-link.php
@@ -1,10 +1,10 @@
 <article class="format <?php echo get_post_format(); ?>">
+	<?php get_template_part('format.date'); ?>
 	<h2>
 		<?php printf('<a href="%s">%s</a>', get_post_meta(get_the_ID(), 'link', true),  get_the_title()); ?>
 	</h2>
 
 	<?php
-		get_template_part('format.date');
 		the_content('Continue reading &raquo;');
 	?>
 </article>
