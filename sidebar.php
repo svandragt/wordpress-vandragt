afd9f587d82750ef3f89679f10329c6eeb958243
Sander van Dragt <sander@vandragt.com>
2016-09-22 13:16:50 +0000

    sidebar

---
+++ sidebar.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The template for the sidebar containing the main widget area
+ */
+?>
+
+<aside id="secondary" class="sidebar widget-area" role="complementary">
+    <?php if ( is_active_sidebar( 'frontpage-1' ) && is_front_page()  ) : ?>
+    		<?php dynamic_sidebar( 'frontpage-1' ); ?>
+    <?php endif; ?>
+
+
+    <?php if ( is_active_sidebar( 'sidebar-1' )  ) : ?>
+    		<?php dynamic_sidebar( 'sidebar-1' ); ?>
+    <?php endif; ?>
+</aside><!-- .sidebar .widget-area -->
