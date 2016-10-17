b92b665784dbd74cf92fb43ef3f14c2476b79c21
Sander van Dragt <sander@vandragt.com>
2016-10-17 15:44:15 +0000

    rename home

---
+++ home-grouped.php
@@ -0,0 +1,35 @@
+<?php
+    /**
+    * Template Name: Category Grouped Timeline
+    */
+ get_header(); ?>
+
+<?php // get_sidebar( 'home' ); ?>
+
+
+<div class="container">
+	<div id="content" role="main" class="index">
+		<?php
+		// todo group by category, then limit to only x per cat; keeping paging ok
+		$category_posts = vd_posts_grouped_by_category(true);
+		foreach ($category_posts as $category => $posts):
+			$category_id = get_cat_ID( $category);
+			$category_link = get_category_link( $category_id );
+			printf('<article class="format-list"><h2 class="category"><a href="%s">%s</a></h2>',$category_link,$category);
+
+			foreach ($posts as $post):
+				setup_postdata($post);
+				get_template_part( 'format.list', get_post_format() );
+			endforeach;
+			print('</article>');
+		endforeach;
+		?>
+
+		<p class="article-footer"><a href="/archives">Archives, search</a></p>
+	</div>
+
+
+
+</div>
+
+<?php get_footer();
