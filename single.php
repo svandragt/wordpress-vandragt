4fdde63f98059657b33b338dc5ae23002f557739
Sander van Dragt <sander@vandragt.com>
2016-10-17 13:48:13 +0000

    new template: frontpage

--- single.php
+++ single.php
@@ -6,6 +6,8 @@
 	    <?php get_template_part( 'format.single', get_post_format() ); ?>
 
 	    <?php get_template_part( 'comments' ); ?>
+	    
+	    <?php edit_post_link( 'Edit This', '<div class="editlink">', '</div>'); ?> 
 	</main>
 
 	<?php //get_sidebar( 'content-bottom' ); ?>
