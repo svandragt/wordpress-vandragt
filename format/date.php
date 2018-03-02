69ac7b4cd440b280f175bc8a193febb491169ccb
Sander van Dragt <sander@vandragt.com>
2018-03-02 15:45:38 +0000

    remove test

--- format/date.php
+++ format/date.php
@@ -1,4 +1,4 @@
 <p class="meta">
-    <a href="<?php the_permalink(); ?>"><?php the_time('F j, Y \a\t g:s A'); ?></a> in... <?php the_category(', '); ?>
-    <?php edit_post_link('Edit This', '<span class="editlink">', '</span>'); ?> SVD
+    <a href="<?php the_permalink(); ?>"><?php the_time('F j, Y \a\t g:s A'); ?></a> in <?php the_category(', '); ?>
+    <?php edit_post_link('Edit This', '<span class="editlink">', '</span>'); ?>
 </p>
