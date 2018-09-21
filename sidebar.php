ac555e767e4c55afb915ee7c28f47f25b835ef36
Sander van Dragt <sander@vandragt.com>
2018-09-21 12:15:23 +0000

    test design

--- sidebar.php
+++ sidebar.php
@@ -6,6 +6,19 @@
 
 <aside id="secondary" class="sidebar widget-area" role="complementary">
 
+      <div id="header">
+    <h1><a href="<?php bloginfo( 'url' ); ?>"><?php bloginfo('name'); ?></a></h1>
+    <nav id="nav">
+			<?php
+				wp_nav_menu( array(
+					'theme_location' => 'primary',
+					'menu_class'     => 'primary-menu',
+					'depth'          => 2,
+				 ) );
+			?>
+    </nav>
+  </div>
+
     <?php if ( is_active_sidebar( 'frontpage-1' ) && is_front_page()  ) : ?>
     		<?php dynamic_sidebar( 'frontpage-1' ); ?>
     <?php endif; ?>
